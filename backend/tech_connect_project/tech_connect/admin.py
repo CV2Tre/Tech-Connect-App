a16539b6cf4bc296766e18c92d3f59a9b9142396
CV2Tre <cvlookin@me.com>
2024-04-05 21:29:48 -0700

    added tags and structure

--- backend/tech_connect_project/tech_connect/admin.py
+++ backend/tech_connect_project/tech_connect/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import JobPosting, Technology,ExperienceLevel
+from .models import JobPosting, Technology,ExperienceLevel,Category 
 
 # Register your models here.
 
 admin.site.register(JobPosting)
 admin.site.register(Technology)
+admin.site.register(Category)
 
 admin.site.register(ExperienceLevel)
--- backend/tech_connect_project/tech_connect/serializers.py
+++ backend/tech_connect_project/tech_connect/serializers.py
@@ -1,20 +1,26 @@
 from rest_framework import serializers
-from .models import Technology, ExperienceLevel, JobPosting
+from .models import JobPosting, Technology, ExperienceLevel, Category
 
 class TechnologySerializer(serializers.ModelSerializer):
     class Meta:
         model = Technology
-        fields = ('id', 'name')
+        fields = ['id', 'name']
 
 class ExperienceLevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExperienceLevel
-        fields = ('id', 'name')
+        fields = ['id', 'name']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
 
 class JobPostingSerializer(serializers.ModelSerializer):
     experience_levels = ExperienceLevelSerializer(many=True, read_only=True)
     technologies = TechnologySerializer(many=True, read_only=True)
+    categories = CategorySerializer(many=True, read_only=True)
 
     class Meta:
         model = JobPosting
-        fields = ('id', 'title', 'description', 'company_name', 'job_type', 'experience_levels', 'salary_range', 'posted_date', 'category', 'technologies')
+        fields = ['id', 'title', 'description', 'company_name', 'job_type', 'experience_levels', 'salary_range', 'posted_date', 'categories', 'technologies']
